393dbfd3d0f9311e66b6d6bdac6037260769464b
yaoMYZ <mozhenyaomyz@163.com>
2020-11-09 20:56:23 +0800

    第一次提交

---
+++ src/main/java/edu/scut/main/Try.java
@@ -0,0 +1,7 @@
+package edu.scut.main;
+
+public class Try {
+	public static void test() {
+		System.out.println("test");
+	}
+}
---
+++ src/test/java/edu/scut/main/Test.java
@@ -0,0 +1,9 @@
+package edu.scut.main;
+
+public class Test {
+	@org.junit.Test
+	public void main() {
+		Try.test();
+	}
+
+}
